d1b22638d1592febd4299b1b148c486880352aef
Nomemb <nkj1205@gmail.com>
2022-07-19 21:27:43 +0900

    결승점 도착 구현
    결승점 도착 시 달리기를 멈추고 시점을 후방으로 전환

--- HyperBCSDProject/Assets/Scripts/PlayerController.cs
+++ HyperBCSDProject/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameMode
+{
+    RunMode,
+    PlacementMode,      // 배치 모드
+
+}
 public class PlayerController : MonoBehaviour
 {
     
@@ -16,6 +22,7 @@ public class PlayerController : MonoBehaviour
     private Vector3 touchStart;     // 터치 시작 지점
     private Vector3 touchEnd;       // 터치 종료 지점
 
+    public GameMode gameMode;
     /*
      * [serializeField]
      * private GameObject Units;    // 움직일 유닛들 오브젝트
@@ -24,12 +31,20 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRigid = GetComponent<Rigidbody>();
+        gameMode = GameMode.RunMode;
     }
 
     void Update()
     {
-        PlayerMove();
-        OnMouse();
+        if(gameMode == GameMode.RunMode)
+        {
+            PlayerMove();
+            OnMouse();
+        }
+        else
+        {
+
+        }
     }
 
     // 기본 이동
@@ -57,5 +72,19 @@ public class PlayerController : MonoBehaviour
     {
         transform.position += new Vector3(touchEnd.x - touchStart.x, 0, 0).normalized * dragDistance;
         yield return null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 결승점에 도달하면
+        if (other.CompareTag("FinishLine"))
+        {
+            // 게임모드를 배치 모드로 바꿈
+            gameMode = GameMode.PlacementMode;
+            // 180도 회전시킴
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+
+            // 카메라, 애니메이션 변경해줘야 함
+        }
     }
 }
