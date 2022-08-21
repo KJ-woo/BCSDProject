030ab01a2678d3a2243cd55cd783982a678c1561
Nomemb <nkj1205@gmail.com>
2022-08-22 02:53:21 +0900

    [feat] 영웅 정렬 및 사망처리

--- HyperBCSDProject/Assets/Scripts/HeroSpawner.cs
+++ HyperBCSDProject/Assets/Scripts/HeroSpawner.cs
@@ -4,16 +4,70 @@ using UnityEngine;
 
 public class HeroSpawner : MonoBehaviour
 {
-    private Hero instantHero;
-    public List<Hero> CurrentHeroList = new List<Hero>();
-    public List<Hero> AddedHeroList = new List<Hero>();
+    [Header("BoxCollider의 x size를 width에 맞춰야 함")]
+    [SerializeField]
+    private int width = 4;
+    [SerializeField]
+    private int height = 8; // maximum = width * height
 
-    // 영웅 선택지 중 하나를 골랐을때 실행되는 함수.
-    public void AddHero(List<Hero> AddHeroList)
+    private GameObject instantHero;
+    private BoxCollider area;
+    private Vector3[] heroesPosition;
+    public List<GameObject> CurrentHeroList = new List<GameObject>();
+    public List<GameObject> AddedHeroList = new List<GameObject>();
+
+    private void Start()
     {
-        CurrentHeroList.AddRange(AddHeroList);
+        area = GetComponent<BoxCollider>();
+        heroesPosition = new Vector3[height * width];
+    }
+    private void Update()
+    {
+        SetHeroesPosition();
+        UpdateHeroesPosition();
+    }
+    // 영웅 선택지 중 하나를 골랐을때 실행되는 함수.
+    public void AddHero(List<GameObject> AddHeroList)
+    {
         AddedHeroList.AddRange(AddHeroList);
         StartCoroutine(InstantiateHero());
+    }
+
+    public void DeleteHero(GameObject hero)
+    {
+        int index = CurrentHeroList.IndexOf(hero);
+        
+        StartCoroutine(DestroyDelay(index));
+    }
+    IEnumerator DestroyDelay(int index)
+    {
+        new WaitForSeconds(2.0f);
+        Destroy(CurrentHeroList[index]);
+        CurrentHeroList.RemoveAt(index);
+        yield return null;
+    }
+    // 초기 자리를 설정하는 함수
+    private void SetHeroesPosition()
+    {
+        Vector3 size = area.size;
+        Vector3 basePosition = transform.position;
+
+        for(int i = 0; i < height * width; ++i)
+        {
+            float posX = basePosition.x - width / 2 + 0.5f + i % width ;
+            float posY = basePosition.y;
+            float posZ = basePosition.z - i / width;
+            heroesPosition[i] = new Vector3(posX, posY, posZ);
+        }
+    }
+
+    // 영웅의 위치를 업데이트해주는 함수
+    private void UpdateHeroesPosition()
+    {
+        for(int i=0;i<CurrentHeroList.Count; ++i)
+        {
+            CurrentHeroList[i].transform.position = heroesPosition[i];
+        }
     }
 
     // 새로 추가된 영웅을 생성한다.
@@ -27,7 +81,7 @@ public class HeroSpawner : MonoBehaviour
             instantHero.transform.parent = transform;
             // 리스트의 해당 인덱스 제거
             AddedHeroList.RemoveAt(0);
-
+            CurrentHeroList.Add(instantHero);
         }
         yield return null;
     }
