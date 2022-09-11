d1d02dec7bd9b617ff24568b389fec03380d087f
kaylahee <kaylahee0410@gmail.com>
2022-09-11 12:48:57 +0900

    [feat] 몬스터 공격 시스템, 애니메이션 추가

---
+++ HyperBCSDProject/Assets/Scripts/Monster/MonsterController.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    public MonsterStat mstat;
+    /*public HeroStat hstat;*/
+
+    public GameObject Monster;
+    private float Dist;
+
+    WaitForSeconds attackCooltime;
+    public Coroutine attackCoroutine;
+    [SerializeField]
+    public float Cooltime;
+
+    // 특정 레이어 지정을 위한 레이어 마스크
+    /*public LayerMask heroLayer;*/
+    private Ray ray;
+    private RaycastHit raycastHit;
+
+    private bool walk;
+    private bool attack_ready;
+    private bool attacking;
+    private bool takedamage;
+    private bool dead;
+
+    public Animator animator;
+
+    // 종료 지점
+    [SerializeField]
+    Vector3 target;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        setAttackCooltimeWaitForSeconds(Cooltime);
+    }
+
+    public void setAttackCooltimeWaitForSeconds(float time)
+    {
+        attackCooltime = new WaitForSeconds(time);
+    }
+
+    void Update()
+    {
+        animator.SetBool("walk", walk);
+        animator.SetBool("attack_ready", attack_ready);
+        animator.SetBool("attack", attacking);
+        animator.SetBool("takeDamage", takedamage);
+        animator.SetBool("die", dead);
+        Dist = Vector3.Distance(GameObject.FindWithTag("Hero").transform.position, Monster.transform.position);
+        // Debug.Log(Dist);
+
+        // 몬스터가 보는 방향 알아내기
+        /*Vector3 look = Monster.transform.TransformDirection(Vector3.forward);*/
+        /*Debug.DrawRay(Monster.transform.position + Vector3.up, look * 10, Color.red);*/
+        /*ray = new Ray(Monster.transform.position + Vector3.up, look);*/
+       
+        MoveCommand();
+        AttackCommand();
+    }
+
+    void MoveCommand()
+    {
+        // 종료 지점까지 몬스터 이동
+        float fMove = Time.deltaTime * mstat.m_speed;
+        transform.position = Vector3.MoveTowards(transform.position, target, fMove);
+
+        // 종료 지점 도달할 경우 걷기 애니메이션 멈추기
+        if (transform.position != target)
+        {
+            walk = true;
+        }
+        else
+        {
+            walk = false;
+            attack_ready = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hero"))
+        {
+            Debug.Log("Attack_Start");
+            /*hstat.hp -= mstat.power;*/
+        }
+    }
+
+    // 공격
+    public void AttackCommand()
+    {
+        /*Vector3 look = Monster.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(Monster.transform.position + Vector3.up, look * 10, Color.red);
+        ray = new Ray(Monster.transform.position + Vector3.up, look);
+        if (Physics.Raycast(ray, 10, heroLayer))
+        {
+            *//*Attacking();*//*
+            Debug.Log(transform.name);
+        }*/
+        // 영웅과 몬스터 간의 거리가 공격 사거리 안에 들어올 경우
+        if (!walk && Dist <= mstat.a_range)
+        {
+            Attacking();
+        }
+        else
+        {
+            CancelAttakcing();
+        }
+    }
+
+    // targetPoint쪽으로 회전시키는 메소드
+    void Rotation(Vector3 targetPoint)
+    {
+        Vector3 relativePosition = targetPoint - transform.position;
+        relativePosition.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(relativePosition, Vector3.up);
+        transform.rotation = rotation;
+    }
+
+    // 공격 시 타겟으로 몸 회전 후 공격 코루틴 실시
+    void Attacking()
+    {
+        Rotation(GameObject.FindWithTag("Hero").transform.position);
+        if(attack_ready)
+        {
+            attackCoroutine = StartCoroutine(AttackingIEnumerator());
+        }
+    }
+
+    // 공격 정지
+    void CancelAttakcing()
+    {
+        if (attacking)
+        {
+            /*StopCoroutine(attackCoroutine);*/
+            attacking = false;
+            attack_ready = true;
+        }
+    }
+
+    public IEnumerator AttackingIEnumerator()
+    {
+        attacking = true;
+        yield return attackCooltime;
+        /*attacking = false;*/
+        attack_ready = true;
+    }
+
+    // 데미지만큼 체력 감소
+    public void OnDamage()
+    {
+        takedamage = true;
+        // 데미지만큼 체력 감소
+        /*mstat.HP -= hstat.damage;*/
+        attack_ready = true;
+        if (mstat.HP <= 0)
+        {
+            Die();
+        }
+    }
+
+    // 사망
+    public void Die()
+    {
+        dead = true;
+        // 몬스터 삭제
+    }
+}
